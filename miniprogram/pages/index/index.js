da3bdd3d6047de94db26e9d43bc9f8c982938512
YangYuesong <E0015005@u.nus.edu>
2020-08-06 16:13:07 +0800

    category list in index

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -9,7 +9,16 @@ Page({
       'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
       'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
     ],
-    category :['Textbook', 'Electornics', 'Clothes', 'Boyfriends', 'Girlfriends', 'Wifes', 'Husbands', 'Trump']
+    category :[
+      {name:'Textbook', icon: '../../images/index/textbook.png'},
+      {name:'Electornics', icon: '../../images/index/electronics.png'}, 
+      {name:'Clothes', icon: '../../images/purchase_on.png'},
+      {name:'Boyfriends', icon: '../../images/purchase_on.png'}, 
+      {name:'Girlfriends', icon: '../../images/purchase_on.png'}, 
+      {name:'Wifes', icon: '../../images/purchase_on.png'}, 
+      {name:'Husbands', icon: '../../images/purchase_on.png'}, 
+      {name:'Trump', icon: '../../images/index/trump.png'}
+    ]
   },
   onLoad: function(){
     wx.cloud.callFunction({
