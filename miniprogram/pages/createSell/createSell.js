b46213488b7790e79b4c6476e0bb91f4a1d71f76
YangYuesong <E0015005@u.nus.edu>
2020-08-07 17:18:11 +0800

    preview image

--- miniprogram/pages/createSell/createSell.js
+++ miniprogram/pages/createSell/createSell.js
@@ -6,11 +6,29 @@ Page({
     category_selection_index:0,
     location_list : ["Utown", "Science", "FASS", "SOC"],
     location_selection_index:0,
+    images : []
   },
+  
   onLoad: function(){
   },
 
+  uploadImages: function(){
+    var images = this.data.images;
+    var image;
+    for (image of images){
+      var cloud_path = "posts/".concat(image);
+      wx.cloud.uploadFile({
+        cloudPath: cloud_path,
+        filePath: image,
+        success : res => {
+          console.log("uploaded", res)
+        }
+        })
+    }
+  },
+
   postSell: function(event){
+    this.uploadImages()
     wx.cloud.callFunction({
       name: "postSell",
       data: {
@@ -41,6 +59,13 @@ Page({
       category_selection_index: event.detail.value
     })
   },
+
+  location_picker: function(event){
+    this.setData({
+      location_selection_index: event.detail.value
+    })
+  },
+
   title_input: function(event){
     // console.log('title is', event.detail.value)
     this.setData({
@@ -52,6 +77,20 @@ Page({
     // console.log('date is', event.detail.value)
     this.setData({
       date: event.detail.value
+    })
+  },
+
+  chooseImage(event){
+    wx.chooseImage({
+      count: 4,
+      sizeType : ['original', 'compressed'],
+      sourceType : ['album', 'camera'],
+      success : res => {
+        this.setData({images : []})
+        const images = this.data.images.concat(res.tempFilePaths);
+        this.data.images = images.length <= 4 ? images : images.slice(0, 4) 
+        this.setData({images: images})
+      }
     })
   }
 })
