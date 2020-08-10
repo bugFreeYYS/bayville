2147940faa4281833b8206dd1c63af2264ff3152
RaymondZXP <mshs.raymond.zhang@gmail.com>
2020-08-10 14:55:43 +0800

    'wanttorestore'

--- miniprogram/pages/createSell/createSell.js
+++ miniprogram/pages/createSell/createSell.js
@@ -1,112 +1,131 @@
 //index.js
 Page({
-  data: {
+  data:{
     title: '',
-    date: '请选择',
-    category_list: ["请选择", "Textbook", "Electronics", "Furniture", "Cosmetics"],
-    category_selection_index: 0,
-    location_list: ["请选择", "Utown", "Science", "FASS", "SOC"],
-    location_selection_index: 0,
-    image_urls: [],
+    date: '',
+    category_list : ["Textbook", "Electronics", "Furniture", "Cosmetics"],
+    category_selection_index:0,
+    location_list : ["Utown", "Science", "FASS", "SOC"],
+    location_selection_index:0,
+    image_urls : [],
     image_urls_cloud: []
   },
+  
+  onLoad: function(){
+    wx.cloud.callFunction({
+      name:"login",
+      success : (res) =>{
+        console.log('retrieved openid');
+        this.setData({
+          openid:res.result.openid
+        })
+      }
+    });
+  },
 
-  onLoad: function () {},
-
-  async uploadImages() {
+  async uploadImages(){
     var that = this;
     var image_urls = this.data.image_urls;
     var image;
-    this.setData({
-      image_urls_cloud: []
-    })
-    for (image of image_urls) {
-      var cloud_path = "posts/".concat(image.split('/')[3]);
+
+    
+    this.setData({image_urls_cloud : []})
+    for (image of image_urls){
+      var date= new Date();
+      var timestamp= date.getTime().toString()
+      var cloud_path = "posts/".concat(image.split('/')[3]+'_').concat(that.data.openid+'_').concat(timestamp);
+      console.log(cloud_path);
       await wx.cloud.uploadFile({
         cloudPath: cloud_path,
         filePath: image,
-      }).then(res => {
+      }).then(res =>{
         const image_urls_cloud = that.data.image_urls_cloud.concat(res.fileID);
-        that.setData({
-          image_urls_cloud: image_urls_cloud
-        });
-      })
+        that.setData({ image_urls_cloud: image_urls_cloud });
+        
+      }) 
     }
-
+    // console.log("3");
   },
 
-  async postSell(event) {
+  async postSell(event){
+    // console.log("1");
     await this.uploadImages();
+    
     wx.cloud.callFunction({
       name: "postSell",
       data: {
         title: event.detail.value.title,
-        price: event.detail.value.price,
+        price:  event.detail.value.price,
         description: event.detail.value.description,
         contact: event.detail.value.contact,
-        category: event.detail.value.category,
-        location: event.detail.value.location,
+        category:  event.detail.value.category,
+        location:  event.detail.value.location,
         transaction_date: event.detail.value.transaction_date,
-        image_urls: this.data.image_urls,
+        image_urls : this.data.image_urls,
         image_urls_cloud: this.data.image_urls_cloud
       },
       success: (res) => {
         console.log('create success!');
         wx.showToast({
-          title: '提交成功',
+          title: 'Success',
           icon: 'success',
           duration: 2000
         });
-        setTimeout(function () {
+        setTimeout(function(){
           wx.reLaunch({
             url: '/pages/index/index',
           })
-        }, 3000)
+          }, 3000
+        )
       }
-
+ 
     })
   },
 
-  title_input: function (event) {
+  title_input: function(event){
+    // console.log('title is', event.detail.value)
     this.setData({
       title: event.detail.value
     })
   },
 
-  category_picker: function (event) {
+  category_picker: function(event){
+    // console.log('category is', event.detail.value)
     this.setData({
       category_selection_index: event.detail.value
     })
   },
 
-  location_picker: function (event) {
+  location_picker: function(event){
     this.setData({
       location_selection_index: event.detail.value
     })
   },
 
+  title_input: function(event){
+    // console.log('title is', event.detail.value)
+    this.setData({
+      location_selection_index: event.detail.value
+    })
+  },
 
-  date_picker: function (event) {
+  date_picker: function(event){
     // console.log('date is', event.detail.value)
     this.setData({
       date: event.detail.value
     })
   },
 
-  chooseImage: function (event) {
+  chooseImage: function(event){
     wx.chooseImage({
       count: 4,
-      sizeType: ['original', 'compressed'],
-      sourceType: ['album', 'camera'],
-      success: res => {
-        this.setData({
-          image_urls: []
-        })
+      sizeType : ['original', 'compressed'],
+      sourceType : ['album', 'camera'],
+      success : res => {
+        this.setData({image_urls : []})
         const image_urls = this.data.image_urls.concat(res.tempFilePaths);
-        this.data.image_urls = image_urls.length <= 4 ? image_urls : image_urls.slice(0, 4)
-        this.setData({
-          image_urls: image_urls
-        })
+        this.data.image_urls = image_urls.length <= 4 ? image_urls : image_urls.slice(0, 4) 
+        this.setData({image_urls: image_urls})
       }
     })
   },
@@ -117,17 +136,9 @@ Page({
       value
     })
   },
-  validateNumber: function (val) {
 
-    // console.log(this.validate(val));
-    return this.validate(val) ? val : val.slice(0, -1);
-
-  },
-
-  validate: function (s) {
-    var rgx = /^[0-9]+\.{0,1}[0-9]{0,2}$/;
-    return s.match(rgx);
+  validateNumber(val) {
+    return val.replace(/\D/g, '')
   }
-
 
 })
