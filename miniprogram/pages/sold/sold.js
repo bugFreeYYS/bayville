c1fda9c3081d5c79e81d12cabefdcbf640bad6a1
RaymondZXP <mshs.raymond.zhang@gmail.com>
2020-08-14 16:56:30 +0800

    'added_delete'

--- miniprogram/pages/sold/sold.js
+++ miniprogram/pages/sold/sold.js
@@ -2,7 +2,6 @@
 
 Page({
   data: {},
-
   onLoad: function () {
     wx.cloud.callFunction({
       name:"getMyPosts",
@@ -19,4 +18,35 @@ Page({
       url: '/pages/editPost/editPost?itemid=' + itemid,
     })
   },
+
+  confirmDelete: function(e){
+    let that = this;
+    console.log('delete');
+    wx.showModal({
+      title: '提示',
+      content: '确认删除',
+      success (res) {
+        if (res.confirm) {
+          that.deletePost(e);
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+  },
+  deletePost: function(e) {
+    let that= this;
+    console.log(e.currentTarget.dataset.itemid);
+    wx.cloud.callFunction({
+      name:"deleteMyPost",
+      data: {
+        itemid: e.currentTarget.dataset.itemid
+      },
+      success: (res) =>{
+        console.log('云端数据已删除');
+        that.onLoad();
+      }
+    })
+  }
 })
